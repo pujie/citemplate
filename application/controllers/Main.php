64c7d8672d2a2c34d40fb788e0f030106954eeab
pujie <puji@gmail.com>
2018-02-15 18:38:57 +0700

    clean main

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -4,14 +4,7 @@ class Main extends CI_Controller{
         parent::__construct();
     }
     function login(){
-/*        require_once './vendor/autoload.php';
-        $loader = new Twig_Loader_Array(array(
-            'index' => 'Hello {{ name }}!',
-        ));
-        $twig = new Twig_Environment($loader);*/
-//        echo $twig->render('test.html', array('name' => 'Fabien'));
         $this->twig->display('login/login', []);
-//        $this->load->view('login/login');
     }
     function index(){
         redirect ('main/login');
