23e52fea2d8388a68e711e7cde1c7b573dcd4ff8
pujie <puji@gmail.com>
2018-02-19 14:14:08 +0700

    add user

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -83,7 +83,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url','string');
 
 /*
 | -------------------------------------------------------------------
@@ -126,4 +126,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('client');
+$autoload['model'] = array('client','user');
